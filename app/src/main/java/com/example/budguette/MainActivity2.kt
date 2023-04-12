992e76d3641c2b41b75761ae31f39a75a6a8e00f
RichardHall57 <richardhall157@gmail.com>
2023-04-11 20:19:24 -0400

    Edit readme test mutipage

--- app/src/main/java/com/example/budguette/MainActivity2.kt
+++ app/src/main/java/com/example/budguette/MainActivity2.kt
@@ -2,6 +2,8 @@ package com.example.budguette
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity2 : AppCompatActivity() {
@@ -9,25 +11,30 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
+        val fragmentManager: FragmentManager = supportFragmentManager
+
+        // define your fragments here
+        val fragment1: Fragment = Home()
+        val fragment2: Fragment = Post()
+        val fragment3: Fragment = Budget()
+        val fragment4: Fragment = Profile()
+
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
 
+        // handle navigation selection
         bottomNavigationView.setOnItemSelectedListener { item ->
+            lateinit var fragment: Fragment
             when (item.itemId) {
-                R.id.home ->
-                    // do something here
-                    true
-                R.id.library ->
-                    // do something here
-                    true
-                R.id.camera ->
-                    // do something here
-                    true
-                R.id.profile ->
-                    // do something here
-                    true
-                else -> true
+                R.id.home -> fragment = fragment1
+                R.id.library -> fragment = fragment2
+                R.id.camera-> fragment = fragment3
+                R.id.profile-> fragment = fragment4
             }
+            fragmentManager.beginTransaction().replace(R.id.frame, fragment).commit()
+            true
         }
 
+        // Set default selection
+        bottomNavigationView.selectedItemId = R.id.home
     }
 }
